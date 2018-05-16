bd37eab4e76548dc0c7b27ccf7819a54b0d9de86
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-05-16 17:24:15 +0200

    Final changes made

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,7 @@ class ReviewsController < ApplicationController
   end
 
   def new
-    @restaurant = Restaurant.find(:restaurant_id)
+    @restaurant = Restaurant.find(params[:restaurant_id])
     # Before creating the view, we need to indentify the specific restaurant
     # it is for in an instance variable that can be passed to the view
     # via the simple_form helper.
@@ -13,11 +13,19 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    # We need the params, because this is the URL that has the id.
     @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    # Here is where we really hook up the review to the specific restaurant
+    # that has been defined in line 19.
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
     # We make a new instance of Review here.
-    @review.restaurant = Restaurant.find(:restaurant_id)
     # We set the restaurant_id comolumn of that review to a specific restaurant.
-    @review.save
     # We save that shit.
     # One can't use create because we need to set the restaurant_id column.
   end
@@ -25,6 +33,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).premit(:content, :rating)
+    params.require(:review).permit(:content, :rating)
   end
 end
