efa6ec565558f333e6b0b6871a47836431dc316a
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-05-16 15:02:27 +0200

    We have random reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,7 +9,14 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    @restaurant = Restaurant.create(restaurant_params)
+    @restaurant = Restaurant.new(restaurant_params)
+    # The params here hooks everything up to the user-submitted data.
+    if @restaurant.save
+      redirect_to restaurants_path
+      # Here we test to see if it can save.
+    else
+      render :new
+    end
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Restaurant.destroy_all
+
+
 require 'faker'
 
 category = ["chinese", "italian", "japanese", "french", "belgian"].sample
-# rating = (0..5).to_a.sample
+rating = (0..5).to_a.sample
 
 10.times do
   restaurant = Restaurant.new(
@@ -20,3 +23,12 @@ category = ["chinese", "italian", "japanese", "french", "belgian"].sample
   restaurant.save!
 end
 
+restaurant_id_array = Restaurant.all.map { |restaurant| restaurant.id }
+
+100.times do
+  review = Review.new(
+    restaurant_id: restaurant_id_array.sample,
+    rating: rating,
+    content: Faker::Lorem.paragraph)
+  review.save!
+end
